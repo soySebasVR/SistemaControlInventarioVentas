4826f4177759f4cbbe061d4cb7a7f309ac940b96
Sebastian Vasquez <sebvas18@gmail.com>
2024-10-04 11:22:40 -0500

    producto

---
+++ src/Producto.java
@@ -0,0 +1,27 @@
+public class Producto {
+    private String nombre;
+    private int cantidad;
+    private double precio;
+
+    public Producto(String nombre, int cantidad, double precio) {
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+}
