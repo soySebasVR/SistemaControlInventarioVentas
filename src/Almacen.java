d1a04364c228201d5670c0e2b4f9cbc59e4c4244
Sebastian Vasquez <sebvas18@gmail.com>
2024-10-04 11:22:53 -0500

    almacen getfromdb

---
+++ src/Almacen.java
@@ -0,0 +1,67 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Almacen {
+    private ArrayList<Producto> productos;
+
+    public Almacen() {
+        this.productos = new ArrayList<Producto>();
+    }
+
+    public Producto getProducto(String nombre) {
+        for (Producto p : productos) {
+            if (p.getNombre().equals(nombre)) {
+                return p;
+            }
+        }
+        System.out.println("Producto no encontrado");
+        return null;
+    }
+
+    public static Almacen getFromDB() throws FileNotFoundException {
+        Almacen tienda = new Almacen();
+        File dbFile = new File("db/db.csv");
+
+        try (Scanner myReader = new Scanner(dbFile)) {
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                String regex = ",";
+                String[] myArray = data.split(regex);
+                tienda.productos.add(new Producto(myArray[0], Integer.valueOf(myArray[1]), Double.valueOf(myArray[2])));
+            }
+            myReader.close();
+        }
+
+        return tienda;
+    }
+
+    public void saveInDB() throws IOException {
+        File dbFile = new File("db/db.csv");
+        try (FileWriter fw = new FileWriter(dbFile)) {
+            for (Producto p : productos) {
+                fw.write(p.getNombre() + "," + p.getCantidad() + "," + p.getPrecio() + "\n");
+            }
+            fw.flush();
+            fw.close();
+            System.out.println("Guardado en BD");
+        } catch (IOException e) {
+            System.out.println("Error al guardar en BD");
+            e.printStackTrace();
+        }
+    }
+
+    public void removeProductos(ArrayList<Producto> prodsRemove) {
+        for (Producto pr : prodsRemove) {
+            for (Producto p : productos) {
+                if (p.getNombre().equals(pr.getNombre())) {
+                    p.setCantidad(p.getCantidad() - pr.getCantidad());
+                    break;
+                }
+            }
+        }
+    }
+}
